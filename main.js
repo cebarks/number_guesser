60f4d432eff7ed3a17e78d1dd4f15751ae07d234
Anten Skrabec <cebarks@gmail.com>
2019-03-09 15:43:57 -0700

    adds customizable range fields

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var gameMax = 100
 var gameMin = 1
-var gameGuess = 0
+var gameMax = 100
+var gameGuess = randomIntBetween(gameMin, gameMax)
 
 $(document).ready(() => {
   gameInit();
@@ -10,7 +10,7 @@ $(document).ready(() => {
 function gameInit() {
   $('#user-guess').on('click', makeGuess);
   $('#clear-guess').on('click', clearGuessInput);
-  $('#reset-game').on('click', () => {location.reload();});
+  $('#reset-game').on('click', () => {location.reload()});
 
   $('#clear-guess').prop('disabled', true);
   $('#reset-game').prop('disabled', true);
@@ -23,10 +23,6 @@ function gameInit() {
     }
   });
 
-  $('#guesses').bind('DOMNodeInserted', () => {
-    $('#reset-game').prop('disabled', false);
-  });
-
   $('#user-number').on('keyup', () => {
     if ($('#user-number').val().length == 0) {
       $('#user-guess').prop('disabled', true);
@@ -34,6 +30,13 @@ function gameInit() {
       $('#user-guess').prop('disabled', false);
     }
   });
+
+  $('#guesses').on('DOMNodeInserted', () => {
+    $('#reset-game').prop('disabled', false);
+  });
+
+  $('#min-guess').on('focusout', () => { gameMin = parseInt($('#min-guess').val()); gameGuess = parseInt(randomIntBetween(gameMin, gameMax)); });
+  $('#max-guess').on('focusout', () => { gameMax = parseInt($('#max-guess').val()); gameGuess = parseInt(randomIntBetween(gameMin, gameMax)); });
 
   gameGuess = randomIntBetween(gameMin, gameMax)
 }
@@ -45,7 +48,11 @@ function makeGuess() {
 }
 
 function addGuess(user_guess) {
-  $('#guesses').append('<li>Guess: ' + user_guess + ' | ' + checkGuess(user_guess) + '</li>');
+  reply = checkGuess(user_guess)
+  $('#guesses').append('<li>Guess: ' + user_guess + ' | ' + reply + '</li>');
+  if (reply === 'BOOM!') {
+    newRound();
+  }
 }
 
 function checkGuess(user_guess) {
@@ -64,6 +71,25 @@ function checkGuess(user_guess) {
   } else if (guess === NaN) {
     return 'Your guess was not a number (NaN).';
   }
+}
+
+function newRound() {
+  alert('You won! The range has now been increased!')
+
+  min = gameMin
+  max = gameMax
+  resetGame()
+  gameMin = min - 10
+  gameMax = max + 10
+
+  gameGuess = randomIntBetween(gameMin, gameMax);
+
+  $('#min-guess').val(gameMin)
+  $('#max-guess').val(gameMax)
+}
+
+function resetGame() {
+  $('#guesses').empty();
 }
 
 function getUserGuess() {
