a1d6cb5a1a930e5910f98c824accade602c1ec70
Anten Skrabec <cebarks@gmail.com>
2019-03-09 14:52:28 -0700

    iteration 2

--- main.js
+++ main.js
@@ -2,10 +2,9 @@ var gameMax = 100
 var gameMin = 1
 var gameGuess = 0
 
-$(document).ready(function() {
+$(document).ready(() => {
   gameInit();
-
-  console.log( 'ready!' );
+  console.log('ready!');
 });
 
 function gameInit() {
@@ -13,6 +12,28 @@ function gameInit() {
   $('#clear-guess').on('click', clearGuessInput);
   $('#reset-game').on('click', () => {location.reload();});
 
+  $('#clear-guess').prop('disabled', true);
+  $('#reset-game').prop('disabled', true);
+
+  $('#user-number').on('keyup', () => {
+    if ($('#user-number').val().length != 0) {
+      $('#clear-guess').prop('disabled', false);
+    } else {
+      $('#clear-guess').prop('disabled', true);
+    }
+  });
+
+  $('#guesses').bind('DOMNodeInserted', () => {
+    $('#reset-game').prop('disabled', false);
+  });
+
+  $('#user-number').on('keyup', () => {
+    if ($('#user-number').val().length == 0) {
+      $('#user-guess').prop('disabled', true);
+    } else {
+      $('#user-guess').prop('disabled', false);
+    }
+  });
 
   gameGuess = randomIntBetween(gameMin, gameMax)
 }
@@ -20,6 +41,7 @@ function gameInit() {
 function makeGuess() {
   addGuess(getUserGuess());
   clearGuessInput();
+  $('#clear-guess').prop('disabled', true);
 }
 
 function addGuess(user_guess) {
@@ -29,14 +51,18 @@ function addGuess(user_guess) {
 function checkGuess(user_guess) {
   var guess = parseInt(user_guess);
 
+  if (guess < gameMin || guess > gameMax) {
+    return 'Your guess was out of bounds.'
+  }
+
   if (guess > gameGuess) {
     return 'That is too high';
   } else if (guess < gameGuess) {
     return 'That is too low';
   } else if (guess === gameGuess) {
     return 'BOOM!';
-  } else {
-    return 'Something went wrong.';
+  } else if (guess === NaN) {
+    return 'Your guess was not a number (NaN).';
   }
 }
 
@@ -46,6 +72,8 @@ function getUserGuess() {
 
 function clearGuessInput() {
   $('#user-number').val('');
+  $('#clear-guess').prop('disabled', true);
+  $('#user-guess').prop('disabled', true);
 }
 
 function randomIntBetween(min, max) {
